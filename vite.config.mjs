d33137b6c29dc48d3a497dbb8857c7ac59be2367
Bjørnar Vister Hansen <bvh@olenbetong.no>
2023-07-03 11:16:09 +0200

    Update dependencies and browser targets

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from "vite";
-
 import appframe from "@olenbetong/appframe-vite";
 import react from "@vitejs/plugin-react";
 
+import { defineConfig } from "vite";
+
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [react(), appframe()],
-  appType: "custom",
-  optimizeDeps: {
-    esbuildOptions: { target: ["es2020"] },
-  },
-  build: { target: ["chrome90", "safari14"] },
+	plugins: [react(), appframe()],
+	appType: "custom",
+	optimizeDeps: {
+		esbuildOptions: { target: ["es2022"] },
+	},
+	build: { target: ["chrome105", "safari15"] },
 }));
