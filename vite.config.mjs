217819fe9a1dea689d9bcfe3c5ce395501877565
Bjørnar Vister Hansen <bvh@olenbetong.no>
2024-10-10 10:04:35 +0200

    Update dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,5 +10,5 @@ export default defineConfig(() => ({
 	optimizeDeps: {
 		esbuildOptions: { target: ["es2022"] },
 	},
-	build: { target: ["chrome105", "safari15"] },
+	build: { target: ["chrome105", "safari16"] },
 }));
